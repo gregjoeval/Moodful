4213dd73a8d7b52f57d49943e22a03eaaa73dc0e
Greg Valentine <gregjoeval@gmail.com>
2020-08-26 01:01:50 -0400

    Create Startup.cs

---
+++ Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+[assembly: FunctionsStartup(typeof(Moodful.Startup))]
+namespace Moodful
+{
+  /// <summary>
+  /// Reference: https://www.c-sharpcorner.com/article/how-to-ilogger-reference-in-startup-in-azure-function/
+  /// </summary>
+  public class Startup : FunctionsStartup
+  {
+    private ILoggerFactory _loggerFactory;
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+      var config = new ConfigurationBuilder().AddJsonFile("local.settings.json", optional: true, reloadOnChange: true).AddEnvironmentVariables().Build();
+      builder.Services.AddLogging();
+      ConfigureServices(builder);
+    }
+    public void ConfigureServices(IFunctionsHostBuilder builder)
+    {
+      _loggerFactory = new LoggerFactory();
+      var logger = _loggerFactory.CreateLogger("Startup");
+      logger.LogInformation("Got Here in Startup");
+      //Do something with builder    
+    }
+  }
+}
