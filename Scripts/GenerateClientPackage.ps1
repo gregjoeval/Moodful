6a127620c43691b0786989e6604ee597a2b6f299
Greg Valentine <gregjoeval@gmail.com>
2020-10-24 14:32:39 -0400

    rename openapi-generator to openapi-generator-cli

--- Scripts/GenerateClientPackage.ps1
+++ Scripts/GenerateClientPackage.ps1
@@ -14,24 +14,24 @@ try {
 }
 
 try {
-    openapi-generator version
+    openapi-generator-cli version
 } catch {
-    Write-Error "`n`n There was an issue getting the version of 'openapi-generator'. `n`n"
+    Write-Error "`n`n There was an issue getting the version of 'openapi-generator-cli'. `n`n"
     exit 1
 }
 
 try {
-    openapi-generator validate -i $('{0}' -f $specificationFileLocation) --recommend
+    openapi-generator-cli validate -i $('{0}' -f $specificationFileLocation) --recommend
 } catch {
-    Write-Error "`n`n There was an issue while 'openapi-generator' was validating the specification file. `n`n"
+    Write-Error "`n`n There was an issue while 'openapi-generator-cli' was validating the specification file. `n`n"
     exit 1
 }
 
 try {
-    openapi-generator generate -i $('{0}' -f $specificationFileLocation) -g typescript-fetch -o $('{0}' -f $outputDirectory) -p npmName=$('{0}' -f $clientName) -p npmVersion=$('{0}' -f $clientVersion) -p withInterfaces=true -p useSingleRequestParameter=true -p prefixParameterInterfaces=true 
+    openapi-generator-cli generate -i $('{0}' -f $specificationFileLocation) -g typescript-fetch -o $('{0}' -f $outputDirectory) -p npmName=$('{0}' -f $clientName) -p npmVersion=$('{0}' -f $clientVersion) -p withInterfaces=true -p useSingleRequestParameter=true -p prefixParameterInterfaces=true 
 } catch {
-    Write-Error "`n`n There was an issue while 'openapi-generator' was generating the client package. Check out the logs above. `n`n"
+    Write-Error "`n`n There was an issue while 'openapi-generator-cli' was generating the client package. Check out the logs above. `n`n"
     exit 1
 }
 
-Write-Host "`n`n 'openapi-generator' successfully created the client package. `n`n"
+Write-Host "`n`n 'openapi-generator-cli' successfully created the client package. `n`n"
